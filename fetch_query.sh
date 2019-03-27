3d83e4e46b7509948224e619b4b4b302aaaccb82
Jingjing Yin <hyesung_love1127@163.com>
2019-03-27 21:42:32 +0800

    fetch google query

--- fetch_query.sh
+++ fetch_query.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 QUERY_CNT=10000
+rm query.txt -rf
 
 sh getquery.sh $QUERY_CNT 3 "" "" 7 "baidu" wap
-python3 gbk_to_utf8.py query.txt baidu
+iconv -f gbk -t utf8 query.txt -c -o sogou
+rm query.txt -rf
 
 sh getquery.sh $QUERY_CNT 3 "" "" 7 "sogou" wap
-#mv query.txt sogou
-python3 gbk_to_utf8.py query.txt baidu
+iconv -f gbk -t utf8 query.txt -c -o baidu
+rm query.txt -rf
+
+sh getquery.sh $QUERY_CNT 3 "" "" 7 "google" pc
+iconv -f gbk -t utf8 query.txt -c -o google
+rm query.txt -rf
