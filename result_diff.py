735d6a2814aa121bdb04554cd6bf3c6ea2e007f6
Jingjing Yin <hyesung_love1127@163.com>
2019-04-22 17:24:57 +0800

    value of res_type add Error state

--- result_diff.py
+++ result_diff.py
@@ -19,10 +19,15 @@ import shutil
 
 fetch_service = "http://snapshot.sogou/agent/wget.php?"
 
-# sogou_wordlist = ['马来西亚官方语言', '平头哥是什么动物', '绿茶婊是什么意思', '电阻的作用', '相对原子质量', 'zuzu化妆品怎么样','冯远征和梁丹妮']
+#sogou_wordlist = ['马来西亚官方语言', '平头哥是什么动物', '绿茶婊是什么意思', '电阻的作用', '相对原子质量', 'zuzu化妆品怎么样','冯远征和梁丹妮']
 
 sogou_wordlist = DataFile.read_file_into_list("./sogou")
 baidu_wordlist = DataFile.read_file_into_list("./baidu")
+
+root_result_dir = "/search/odin/yinjingjing/lizhi_result/"  #linux
+#root_result_dir = ""  #windows
+today = datetime.date.today().strftime("%Y-%m-%d")
+scene_dir = ['bd_lizhi_sg_other', 'sg_lizhi_bd_baike', 'sg_lizhi_bd_official', 'sg_lizhi_bd_other', 'sg_lizhi_bd_lizhi']
 
 '''
 def fetch_page(engine, query):
@@ -37,8 +42,8 @@ def fetch_page(engine, query):
 
 
 def sg_fetch_page(query):
-    url = fetch_service + "engine=wap_sogou" + "&query=" + quote(query)
-    #url = "http://wap.sogou.com.inner/web/searchList.jsp?keyword=" + quote(query)
+    #url = fetch_service + "engine=wap_sogou" + "&query=" + quote(query)
+    url = "http://wap.sogou.com.inner/web/searchList.jsp?keyword=" + quote(query)
     #print(url,query)
     try:
         res = requests.get(url)
@@ -59,11 +64,14 @@ def bd_fetch_page(query):
         log_error('[bd_fetch_res]: %s' % err)
 
 
-def fetch_and_analysis(word_list, queue):
-    #同一个query在搜狗侧和百度侧分别抓取并对结果分类
+def fetch_and_analysis(word_list, query_from, queue):
+    #搜狗召回立知，百度为Other，抽样保存现场时计数使用
+    index = 0
+
+    #同一个query在搜狗侧和百度侧分别抓取并对结果分类， query_from表示用的是搜狗词还是百度词，用于保存现场到对应目录
     for query in word_list:
         try:
-            print(query)
+            #print(query)
             #fetch from sogou
             sogou_page = sg_fetch_page(query)
             sg_ret = parse_sogou.parse_sogou(sogou_page)
@@ -75,37 +83,51 @@ def fetch_and_analysis(word_list, queue):
             #print("bd_ret=%s" % bd_ret)
 
             sg_res_type = sg_ret['res_type']
-            sg_res_err = sg_ret['error']
+            #sg_res_err = sg_ret['error']
             bd_res_type = bd_ret['res_type']
-            bd_res_err = bd_ret['error']
-            #queue的内容为线程状态、engine、搜狗结果类别、百度结果类别
-            queue.put(("working", "SG", sg_res_type, bd_res_type))
-            queue.put(("working", "BD", sg_res_type, bd_res_type))
+            #bd_res_err = bd_ret['error']
 
+            #queue的内容为线程状态、engine、查询词、搜狗结果类别、百度结果类别
+            queue.put(("working", "SG", query, sg_res_type, bd_res_type))
+            queue.put(("working", "BD", query, sg_res_type, bd_res_type))
 
+            ###其他现场的抓图先注掉，先抓两方都是立知的
             #如果百度召回立知，搜狗正常抓到结果且结果为其他，保留现场
-            if bd_res_type == 'Lizhi' and not sg_res_err and not sg_res_type:
-                my_screenshot.get_sogou_screenshot(query, "")
-                my_screenshot.get_baidu_screenshot(query, "")
+            if bd_res_type == 'Lizhi' and sg_res_type == 'Other':
+                pass
+                ###my_screenshot.get_sogou_screenshot(query, "/".join([root_result_dir, today, query_from, "bd_lizhi_sg_other"]))
+                ###my_screenshot.get_baidu_screenshot(query, "/".join([root_result_dir, today, query_from, "bd_lizhi_sg_other"]))
 
             #如果搜狗召回立知，百度正常抓到结果，且结果为非立知结果，保留现场
             if sg_res_type == 'Lizhi' and bd_res_type == 'Baike':
-                my_screenshot.get_sogou_screenshot(query, "")
-                my_screenshot.get_baidu_screenshot(query, "")
+                pass
+                ###my_screenshot.get_sogou_screenshot(query, "/".join([root_result_dir, today, query_from, "sg_lizhi_bd_baike"]))
+                ###my_screenshot.get_baidu_screenshot(query, "/".join([root_result_dir, today, query_from, "sg_lizhi_bd_baike"]))
 
             if sg_res_type == 'Lizhi' and bd_res_type == 'Official':
-                my_screenshot.get_sogou_screenshot(query, "")
-                my_screenshot.get_baidu_screenshot(query, "")
+                pass
+                ###my_screenshot.get_sogou_screenshot(query, "/".join([root_result_dir, today, query_from, "sg_lizhi_bd_official"]))
+                ###my_screenshot.get_baidu_screenshot(query, "/".join([root_result_dir, today, query_from, "sg_lizhi_bd_official"]))
 
-            if sg_res_type == 'Lizhi' and not bd_res_err and not bd_res_type:
-                my_screenshot.get_sogou_screenshot(query, "")
-                my_screenshot.get_baidu_screenshot(query, "")
+            if sg_res_type == 'Lizhi' and bd_res_type == 'Other':
+                #抽样保存现场
+                if index % 15 == 0:
+                    pass
+                    ###my_screenshot.get_sogou_screenshot(query, "/".join([root_result_dir, today, query_from, "sg_lizhi_bd_other"]))
+                    ###my_screenshot.get_baidu_screenshot(query, "/".join([root_result_dir, today, query_from, "sg_lizhi_bd_other"]))
+                index += 1
+
+            #给静军提供两方都是立知结果的数据
+            if sg_res_type == 'Lizhi' and bd_res_type == 'Lizhi':
+                my_screenshot.get_sogou_first_screenshot(query, "/".join([root_result_dir, today, query_from, "sg_lizhi_bd_lizhi"]))
+                my_screenshot.get_baidu_first_screenshot(query, "/".join([root_result_dir, today, query_from, "sg_lizhi_bd_lizhi"]))
+
 
         except Exception as err:
             print("[fetch_and_analysis]:%s" % err)
             continue
 
-    queue.put(("quit", None, None, None))
+    queue.put(("quit", None, None, None, None))
 
 
 def split_wordlist(wordlist, n, i):
@@ -122,7 +144,7 @@ def split_wordlist(wordlist, n, i):
         print("[split_wordlist]:%s" % err)
 
 
-def parallel_process(wordlist):
+def parallel_process(wordlist, query_from):
 
     cores = multiprocessing.cpu_count()
     print(cores)
@@ -132,28 +154,32 @@ def parallel_process(wordlist):
 
     static_res = dict()
     #搜狗首条召回情况
-    static_res['Sogou'] = {'Lizhi': 0, 'VR': 0, 'Baike': 0, 'Official': 0, 'Other': 0}
+    static_res['Sogou'] = {'Lizhi': 0, 'VR': 0, 'Baike': 0, 'Official': 0, 'Other': 0, 'Error': 0}
     #百度首条召回情况
-    static_res['Baidu'] = {'Lizhi': 0, 'VR': 0, 'Baike': 0, 'Official': 0, 'Other': 0}
+    static_res['Baidu'] = {'Lizhi': 0, 'VR': 0, 'Baike': 0, 'Official': 0, 'Other': 0, 'Error': 0}
 
     #搜狗首条为立知结果的词，在百度的召回情况
-    static_sg_lizhi = {'Lizhi': 0, 'VR': 0, 'Baike': 0, 'Official': 0, 'Other': 0}
+    static_sg_lizhi = {'Lizhi': 0, 'VR': 0, 'Baike': 0, 'Official': 0, 'Other': 0, 'Error': 0}
     #百度首条为立知结果的词，在搜狗的召回情况
-    static_bd_lizhi = {'Lizhi': 0, 'VR': 0, 'Baike': 0, 'Official': 0, 'Other': 0}
+    static_bd_lizhi = {'Lizhi': 0, 'VR': 0, 'Baike': 0, 'Official': 0, 'Other': 0, 'Error': 0}
 
     try:
         #生成与核数对应的进程数，多进程执行fetch_and_analysis，将词表按进程数切分，每个进程处理对应的词表
         for i in range(cores):
-            p = multiprocessing.Process(target=fetch_and_analysis, args=(split_wordlist(wordlist, cores, i), queue))
+            p = multiprocessing.Process(target=fetch_and_analysis, args=(split_wordlist(wordlist, cores, i), query_from, queue))
             p.start()
             p_list.append(p)
 
         while True:
-            #queue的内容为线程状态、engine、搜狗结果类别、百度结果类别
-            (status, engine, sg_ret, bd_ret) = queue.get()
-            print(status, engine, sg_ret, bd_ret)
+            #queue的内容为线程状态、engine、查询词、搜狗结果类别、百度结果类别
+            (status, engine, query, sg_ret, bd_ret) = queue.get()
+            print(status, engine, query, sg_ret, bd_ret)
+            #insert_sql = '''insert into diff_result\
+                          #(word, queryfrom, fetchfrom, lizhi, vr, baike, official, other, error, resolve) values \
+                          #(word, query_from, engine, );'''
 
             if 'SG' == engine:
+
                 if "Lizhi" == sg_ret:
                     static_res['Sogou']['Lizhi'] += 1
 
@@ -166,8 +192,10 @@ def parallel_process(wordlist):
                         static_sg_lizhi['Baike'] += 1
                     elif 'Official' == bd_ret:
                         static_sg_lizhi['Official'] += 1
-                    else:
+                    elif 'Other' == bd_ret:
                         static_sg_lizhi['Other'] += 1
+                    elif "Error" == bd_ret:
+                        static_sg_lizhi['Error'] += 1
 
                 elif "VR" == sg_ret:
                     static_res['Sogou']['VR'] += 1
@@ -175,10 +203,13 @@ def parallel_process(wordlist):
                     static_res['Sogou']['Baike'] += 1
                 elif "Official" == sg_ret:
                     static_res['Sogou']['Official'] += 1
-                else:
+                elif 'Other' == sg_ret:
                     static_res['Sogou']['Other'] += 1
+                elif "Error" == sg_ret:
+                    static_res['Sogou']['Error'] += 1
 
             elif 'BD' == engine:
+
                 if "Lizhi" == bd_ret:
                     static_res['Baidu']['Lizhi'] += 1
 
@@ -191,8 +222,10 @@ def parallel_process(wordlist):
                         static_bd_lizhi['Baike'] += 1
                     elif 'Official' == sg_ret:
                         static_bd_lizhi['Official'] += 1
-                    else:
+                    elif 'Other' == sg_ret:
                         static_bd_lizhi['Other'] += 1
+                    elif 'Error' == sg_ret:
+                        static_bd_lizhi['Error'] += 1
 
                 elif "VR" == bd_ret:
                     static_res['Baidu']['VR'] += 1
@@ -200,8 +233,11 @@ def parallel_process(wordlist):
                     static_res['Baidu']['Baike'] += 1
                 elif "Official" == bd_ret:
                     static_res['Baidu']['Official'] += 1
-                else:
+                elif "Other" == bd_ret:
                     static_res['Baidu']['Other'] += 1
+                elif "Error" == bd_ret:
+                    static_res['Baidu']['Error'] += 1
+
 
             if status == "quit":
                 quit_count += 1
@@ -216,6 +252,7 @@ def parallel_process(wordlist):
 
     except Exception as err:
         print("[parallel_process]:%s" % err)
+
 
 def gen_result_dir(dirstr):
     try:
@@ -233,36 +270,47 @@ def gen_result_dir(dirstr):
         else:
             os.mkdir(dirstr)
 
+
 if __name__ == '__main__':
+
+    #监控介绍
+    monitor_info = '''
+    1、该监控主要是对比搜狗搜索和百度搜索首条结果的召回情况，判断首条结果的类别，包括：立知、VR、百科、官网和其他结果；<br />
+    2、词源：分别从搜狗和百度侧取10000词；<br />
+    3、搜狗的立知结果包括：首条有立知icon的结果，首条没有立知icon但是属于立知医疗的结果；<br />
+    4、百度的立知结果包括：精选摘要、优质问答、图谱结果；<br />
+    5、由于无法区分百度的VR结果和普通结果，故将其统一归类为“其他结果”；<br />
+    6、页面抓取采用的是数据抓取组提供的代理服务，有时会存在抓不到结果的情况，将此归类为“ERROR”；<br />
+    7、对开发同学关注的几种情况，保存现场，包括：百度首条立知-搜狗首条其他、搜狗首条立知-百度首条百科/官网/其他，<br />
+       搜狗首条立知-百度首条其他，抽样保存现场，其他全量保存现场；<br />
+    8、由于抓取结果无css样式，保存现场通过实际查询+截图实现。由于搜索结果可能会不稳定，导致统计数据和截图现场存在偏差；
+    '''
 
     #多进程抓取和数据分析
     start = time.time()
 
-    today = datetime.date.today().strftime("%Y-%m-%d")
-    scene_dir = ['bd_lizhi_sg_other', 'sg_lizhi_baidu_lizhi', 'sg_lizhi_baidu_baike', 'sg_lizhi_baidu_official', 'sg_lizhi_baidu_other']
-
     #生成搜狗词源的现场目录
     for dir in scene_dir:
-        gen_result_dir(today + "/sogou/" + dir)
+        gen_result_dir(root_result_dir + today + "/sogou/" + dir)
 
     #生成百度词源的现场目录
     for dir in scene_dir:
-        gen_result_dir(today + "/baidu/" + dir)
+        gen_result_dir(root_result_dir + today + "/baidu/" + dir)
 
     #词源为搜狗词，返回三个字典：1、在搜狗/百度的召回情况 2、搜狗首条为立知的词，在百度的召回情况 3、百度首条为立知的词，在搜狗的召回情况
-    sogou_word_static_res, sogou_word_sg_lizhi, sogou_word_bd_lizhi = parallel_process(sogou_wordlist)
+    sogou_word_static_res, sogou_word_sg_lizhi, sogou_word_bd_lizhi = parallel_process(sogou_wordlist, "sogou")
     print("sogou word:%s\t%s\t%s" % (sogou_word_static_res, sogou_word_sg_lizhi, sogou_word_bd_lizhi))
 
     #词源为百度词，返回三个字典：1、在搜狗/百度的召回情况 2、搜狗首条为立知的词，在百度的召回情况 3、百度首条为立知的词，在搜狗的召回情况
-    baidu_word_static_res, baidu_word_sg_lizhi, baidu_word_bd_lizhi = parallel_process(baidu_wordlist)
+    baidu_word_static_res, baidu_word_sg_lizhi, baidu_word_bd_lizhi = parallel_process(baidu_wordlist, "baidu")
     print("baidu word:%s\t%s\t%s" % (baidu_word_static_res, baidu_word_sg_lizhi, baidu_word_bd_lizhi))
 
     end = time.time()
 
     #汇总邮件内容
-    total_table_head = ['数据抓取', '首条为立知', '首条为VR', '首条为百科', '首条为官网', '首条为其他']
-    partial_table_head = ['首条为立知', '首条为VR', '首条为百科', '首条为官网', '首条为其他']
-    static_item = ['Lizhi', 'VR', 'Baike', 'Official', 'Other']
+    total_table_head = ['数据抓取', '首条为立知', '首条为VR', '首条为百科', '首条为官网', '首条为其他', 'ERROR']
+    partial_table_head = ['首条为立知', '首条为VR', '首条为百科', '首条为官网', '首条为其他', 'ERROR']
+    static_item = ['Lizhi', 'VR', 'Baike', 'Official', 'Other', 'Error']
 
     sogou_word_content = "" #搜狗词源的邮件结果内容
     baidu_word_content = "" #百度词源的邮件结果内容
@@ -272,27 +320,40 @@ if __name__ == '__main__':
     #搜狗词总量
     sogou_word_content += Template.html_p("搜狗日志取词总量：" + str(len(sogou_wordlist)))
     #搜狗词在搜狗/百度的召回情况
-    sogou_word_content += Template.double_dict_to_html_table(sogou_word_static_res, total_table_head, static_item)
+    sogou_word_content += Template.double_dict_to_html_table(sogou_word_static_res, len(sogou_wordlist), total_table_head, static_item)
     #搜狗首条为立知的词，在百度的召回情况
     sogou_word_content += Template.html_p("搜狗首条为立知的词，在百度的召回情况:")
     sogou_word_content += Template.single_dict_to_html_table(sogou_word_sg_lizhi, partial_table_head, static_item)
+    #相关现场地址
+    sogou_word_content += Template.html_a_link("/".join(['http://10.144.96.115/lizhi_result', today, 'sogou', 'sg_lizhi_bd_baike']), "搜狗首条立知-百度首条百科")
+    sogou_word_content += Template.html_a_link("/".join(['http://10.144.96.115/lizhi_result', today, 'sogou', 'sg_lizhi_bd_official']), "搜狗首条立知-百度首条官网")
+    sogou_word_content += Template.html_a_link("/".join(['http://10.144.96.115/lizhi_result', today, 'sogou', 'sg_lizhi_bd_other']), "搜狗首条立知-百度首条其他")
     #百度首条为立知的词，在搜狗的召回情况
     sogou_word_content += Template.html_p("百度首条为立知的词，在搜狗的召回情况:")
     sogou_word_content += Template.single_dict_to_html_table(sogou_word_bd_lizhi, partial_table_head, static_item)
+    #相关现场地址
+    sogou_word_content += Template.html_a_link("/".join(['http://10.144.96.115/lizhi_result', today, 'sogou', 'bd_lizhi_sg_other']), "百度首条立知-搜狗首条其他")
+
 
     #百度词总量
     baidu_word_content += Template.html_p("百度搜索取词总量：" + str(len(baidu_wordlist)))
     #百度词在搜狗/百度的召回情况
-    baidu_word_content += Template.double_dict_to_html_table(baidu_word_static_res, total_table_head, static_item)
+    baidu_word_content += Template.double_dict_to_html_table(baidu_word_static_res, len(baidu_wordlist), total_table_head, static_item)
     #搜狗首条为立知的词，在百度的召回情况
     baidu_word_content += Template.html_p("搜狗首条为立知的词，在百度的召回情况:")
     baidu_word_content += Template.single_dict_to_html_table(baidu_word_sg_lizhi, partial_table_head, static_item)
+    #相关现场地址
+    baidu_word_content += Template.html_a_link("/".join(['http://10.144.96.115/lizhi_result', today, 'baidu', 'sg_lizhi_bd_baike']), "搜狗首条立知-百度首条百科")
+    baidu_word_content += Template.html_a_link("/".join(['http://10.144.96.115/lizhi_result', today, 'baidu', 'sg_lizhi_bd_official']), "搜狗首条立知-百度首条官网")
+    baidu_word_content += Template.html_a_link("/".join(['http://10.144.96.115/lizhi_result', today, 'baidu', 'sg_lizhi_bd_other']), "搜狗首条立知-百度首条其他")
     #百度首条为立知的词，在搜狗的召回情况
     baidu_word_content += Template.html_p("百度首条为立知的词，在搜狗的召回情况:")
     baidu_word_content += Template.single_dict_to_html_table(baidu_word_bd_lizhi, partial_table_head, static_item)
+    #相关现场地址
+    baidu_word_content += Template.html_a_link("/".join(['http://10.144.96.115/lizhi_result', today, 'baidu', 'bd_lizhi_sg_other']), "百度首条立知-搜狗首条其他")
 
     report_tmp_path = "lizhi_monitor.html"
-    report_content = "".join([Template.html_general_css(), Template.html_h3_title("立知效果-召回对比"), runtime_html,
+    report_content = "".join([Template.html_general_css(), Template.html_h3_title("立知效果-召回对比"), Template.html_p_spe(monitor_info), runtime_html,
                               sogou_word_content, baidu_word_content])
     DataFile.write_full_file(report_tmp_path, report_content)
 
